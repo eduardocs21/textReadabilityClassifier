a7bb0a4d1525fee0affff19f621cdcf7ea05cebe
3scapeX <78601930+3scapeX@users.noreply.github.com>
2021-03-08 11:57:46 +0100

    globalised variables

--- grammar.py
+++ grammar.py
@@ -26,12 +26,20 @@ pre_per_pro = []
 pa_per_pro = []
 fu_per_pro = []
 
+# basic data
+text: str
+pos_text: list
+parsed_text: list
 
-def check_grammar_klp7(text):
-    # preparation (convert to string, generate pos-tags and change all text to lowercase)
-    text = "Singing is my hobby. I love singing."
-    text = str(text)
-    pos_text = nltk.pos_tag(nltk.word_tokenize(text))
+
+def check_grammar_klp7(raw_text):
+    # preparation (convert to string, generate and print pos-tags and dependency parses)
+    global text
+    global pos_text
+    global parsed_text
+    # raw_text = "Singing is my hobby. I love singing."
+    text = str(raw_text)
+    pos_text = nltk.pos_tag(nltk.word_tokenize(raw_text))
     print("POS-Tags: " + str(pos_text))
     dep_parser = CoreNLPDependencyParser(url='http://localhost:9000')
     parsed_text = dep_parser.parse(text.split())
@@ -42,29 +50,29 @@ def check_grammar_klp7(text):
 
     # 1)
     # plural of nouns
-    print(search_postags(pos_text, ['NNS', 'NNPS'], "plural of nouns"))
+    print(search_postags(['NNS', 'NNPS'], "plural of nouns"))
     print()
 
     # s-genitive
-    print(search_postags(pos_text, ['POS'], "possessive ending (s-Genitiv)"))
+    print(search_postags(['POS'], "possessive ending (s-Genitiv)"))
     print()
 
     # adverbs (of frequency)
-    print(search_postags(pos_text, ['RB'], "adverbs"))
+    print(search_postags(['RB'], "adverbs"))
     print()
 
     # comparative adjectives and adverbs
-    print(search_postags(pos_text, ['JJR', 'JJS', 'RBR', 'RBS'], "comparative adjectives (and adverbs)"))
+    print(search_postags(['JJR', 'JJS', 'RBR', 'RBS'], "comparative adjectives (and adverbs)"))
     print()
 
     # 2)
     # personal pronouns
     # print(search('personal pronouns', ['i', 'you', 'he', 'she', 'it', 'we', 'they'], text.lower()))
-    print(search_postags(pos_text, ['PRP'], "personal pronoun"))
+    print(search_postags(['PRP'], "personal pronoun"))
     print()
 
     # modal verbs (can/can't)
-    print(search_postags(pos_text, ['MD'], "modal verbs"))
+    print(search_postags(['MD'], "modal verbs"))
     print()
 
     # imperatives
@@ -74,7 +82,7 @@ def check_grammar_klp7(text):
     # there + be
 
     # possessive determiners (different to absolute possessive pronouns)
-    poss_det, poss_pro = search_possessive_pronouns(pos_text)
+    poss_det, poss_pro = search_possessive_pronouns()
 
     # if there are matches, print it out and return a dictionary containing word frequencies
     if poss_det:
@@ -92,11 +100,10 @@ def check_grammar_klp7(text):
     # Mengenangaben
 
     # present progressive
-    search_tense_aspects(parsed_text)
-    print(fu_gt)
+    search_tense_aspects()
 
     # simple past
-    print(search_postags(pos_text, ['VBD'], "simple past"))
+    print(search_postags(['VBD'], "simple past"))
     print()
 
     # this/that/these/those
@@ -119,7 +126,7 @@ def check_grammar_klp9():
     print('checking')
 
 
-def search_postags(pos_text, pos_tags, name):
+def search_postags(pos_tags, name):
     words = []
     frequency = {}
 
@@ -144,7 +151,7 @@ def search_postags(pos_text, pos_tags, name):
 
 # searches for possessive pronouns and differentiates them between possessive determiners and pronouns
 # returns separate word lists (all lowercase)
-def search_possessive_pronouns(pos_text):
+def search_possessive_pronouns():
     pd_words = []
     pp_words = []
 
@@ -175,15 +182,21 @@ def search_possessive_pronouns(pos_text):
     return pd_words, pp_words
 
 
-def search_tense_aspects(parsed_text):
-    # search every occurrence of every tense aspect in the text and save it in a list
-    # form (example): return [ [], ["walking", "climbing"], [], [], ["formed", "sung", "taken"]]
+def search_tense_aspects():
+    # search every occurrence of every tense aspect in the text and save it in the according global list
+    # form (example): pre_pro = ["walking", "climbing"], pa_par = ["formed", "sung", "taken"]]
     # abbreviations: fu = future, pa = past, pre = present
     #               pro = progressive, per = perfect, si = simple, gt = going-to, part = participle
 
-    print("BOOOM")
-    print(type(parsed_text))
+    print(text)
     for parse in parsed_text:
-        for x, y, z in parse:
-            print(x, y, z)
+        for x, y, z in parse.triples():
+            print(x)
+
+    print("POS-Tags: " + str(pos_text))
+
+    print("Dependency Parsing: " + str(
+        [[(governor, dep, dependent) for governor, dep, dependent in parse.triples()] for parse in parsed_text]))
+
+    print([[(governor, dep, dependent) for governor, dep, dependent in parse.triples()] for parse in parsed_text])
 
