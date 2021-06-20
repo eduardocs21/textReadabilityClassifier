4b346bdd21ba07e6495065748b6b432dd1d86376
3scapeX <78601930+3scapeX@users.noreply.github.com>
2021-06-20 16:35:34 +0200

    code cleanup and readability script
    moved readability methods to a new script (readability comparison) and other cleanups to make the code and the output better readable

--- trcMain.py
+++ trcMain.py
@@ -1,102 +1,54 @@
-from readability import Readability
+import readability_comparison
 import nltk
 # nltk.download('punkt')
 import os
 import svm_evaluation
 import grammar
 import vocabulary
-import re
 
+# configuration / user input
 training_dirPath = 'training_data'
 test_dirPath = 'test_data'
 path_additional_vocabulary = 'vocabulary_data'
 numberOfFoldsForCrossVal = 10
-grade = 8
-
-data_metrics = []
-data_grades = []
-training_vocabulary = {"."} # define as a set
-text: str
+grade = 6
 
 
-def training_readability():
-    # calculate scores for training data
-    global text
-    global training_vocabulary
-
-    for file in os.listdir(training_dirPath):
-        with open(training_dirPath + '/' + file, encoding='utf-8') as f:
-            text = f.read()
-            f.close()
-
-        # get Meta-Data through file name: ID (arbitrary), Grade (as set by the textbook authors),
-        # Format (read/listen), Type (plain/blog/song/dialog/...)
-        textGrade, textID, textFormat, textType = file.split("_")
-        textType = textType.split(".")[0]
-
-        # filter text types that arent suitable for this analysis
-        if textType in ('dialog', 'cloze', 'mixed', 'german', 'poem', 'song', 'chants', 'bilingualmodule'):
-            continue
-
-        readab = Readability(text)
+training_vocabulary = {"."}  # defined as a set
+print('input grade: ' + str(grade) + '\n')
 
 
-        # calculate readability scores and save as data
-        data_metrics.append(
-            [readab.flesch_kincaid().score, readab.flesch().score, readab.gunning_fog().score, readab.coleman_liau().score,
-             readab.dale_chall().score, readab.ari().score, readab.linsear_write().score, readab.spache().score])
-
-        data_grades.append(textGrade)
-
-
-training_readability()
-# print(data_metrics)
-# print(data_grades)
-print('size of used data set: ' + str(len(data_metrics)))
-
-# cross-validation
+# cross-validation //TODO uncomment
 # scores = svm_evaluation.crossValidation(data_metrics, data_grades, numberOfFoldsForCrossVal)
 # accuracy = sum(scores) / numberOfFoldsForCrossVal
 # print('f1-score = ' + str(accuracy))
 
 
-# prediction of new data
-
-
+# prediction of texts in test data directory
 for file in os.listdir(test_dirPath):
     with open(test_dirPath + '/' + file, encoding='utf-8') as f:
         text = f.read()
         f.close()
 
-    # READABILITY: compare readability of test text to training data
-
-    r = Readability(text)
-
-    test_metrics = [[r.flesch_kincaid().score, r.flesch().score, r.gunning_fog().score, r.coleman_liau().score,
-                     r.dale_chall().score, r.ari().score, r.linsear_write().score, r.spache().score]]
-
     print()
     print('Evaluation of file: ' + file)
     print('---------')
+
+    # READABILITY: compare readability of test text to training data
     print('Readability:')
-    print('scores: ' + str(test_metrics))
-    print('difficulty of input text is similar to a text from grade: ' + str(svm_evaluation.predict(
-       data_metrics, data_grades, test_metrics)))
-    print('---------')
-    print('Grammar: ')
+    data_metrics, data_grades = readability_comparison.training_readability(training_dirPath)
+    print('size of used data set: ' + str(len(data_metrics)))
+    readability_comparison.compare(text, grade)
+    print('--------- \n')
 
     # GRAMMAR: grammar checking of new data
-    # grammar.check_grammar(text)   # TODO uncomment
-
-    print('---------')
-    print('Vocabulary: ')
+    print('Grammar: ')
+    grammar.check_grammar(text, grade)
+    print('\n---------')
 
     # VOCABULARY: check for unknown words
+    print('Vocabulary: ')
     training_vocabulary = vocabulary.add_training_vocabulary(training_dirPath, grade, training_vocabulary)
     training_vocabulary = vocabulary.add_additional_vocabulary(path_additional_vocabulary, training_vocabulary)
     vocabulary.print_unknown_words(text, training_vocabulary)
-
     print('---------' + '\n' + '\n' + '\n' + '\n' + '\n')
-
-
-
